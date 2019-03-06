ed66446f1d6a027c989ea41b6139b053c6b54711
Nils Deckert <42399937+NilsDeckert@users.noreply.github.com>
2019-03-06 18:47:11 +0100

    Adding comments

--- mqtt_publisher.py
+++ mqtt_publisher.py
@@ -1,12 +1,11 @@
-import paho.mqtt.publish as publish
-import time
-import datetime
-import random
+import paho.mqtt.publish as publish     #Needed for mqtt
+import time                             #Needed for 'cooldown'
+import random                           #Only needed for this example to generate random numbers, not needed in actual project
 
-MQTT_SERVER = "192.168.178.66"
-MQTT_PATH = "test_channel"
+MQTT_SERVER = "192.168.178.66"          #Local IP-Address of the Raspberry Pi 2
+MQTT_PATH = "test_channel"              #Channel the data is being sent in
 
-while True:
+while True:                             #Infinite Loop
 
-        publish.single(MQTT_PATH, random.randint(1,10), hostname=MQTT_SERVER)
-        time.sleep(15)
+        publish.single(MQTT_PATH, random.randint(1,10), hostname=MQTT_SERVER)   #Sending a random Number between 1 and 10
+        time.sleep(15)                                                          #Waiting 15seconds until the next loop
