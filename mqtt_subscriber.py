6cf9b55bba16244a8b3a1d26f9364e5689737b01
Nils Deckert <42399937+NilsDeckert@users.noreply.github.com>
2019-03-06 18:55:47 +0100

    Adding Comments

--- mqtt_subscriber.py
+++ mqtt_subscriber.py
@@ -1,12 +1,12 @@
-import paho.mqtt.client as mqtt
-import datetime
-import mysql.connector
+import paho.mqtt.client as mqtt                                                 #Needed for mqtt
+import datetime                                                                 #Need for timestamps
+import mysql.connector                                                          #Needed to connect to MySQL
 
 
-MQTT_SERVER = "localhost"               #MQTT Server (this Raspi)
-MQTT_PATH = "test_channel"              #MQTT Channel
+MQTT_SERVER = "localhost"                                                       #MQTT Server (this Raspi)
+MQTT_PATH = "test_channel"                                                      #MQTT Channel
 
-mydb = mysql.connector.connect(         #MySQL login details
+mydb = mysql.connector.connect(                                                 #MySQL login details
         host="188.**.**.***",
         port="3306",
         user="***063_pi",
@@ -21,18 +21,18 @@ def on_connect(client, userdata, flags, rc):
 
         client.subscribe(MQTT_PATH)
 
-def on_message(client, userdata, msg):
+def on_message(client, userdata, msg):  #Triggers whenever a new message is being received
         localtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        date = datetime.datetime.now().strftime('%Y.%m.%d')
-        timeNow = datetime.datetime.now().strftime('%H:%M:%S')
-        msg.payload = msg.payload.decode("utf-8")
+        date = datetime.datetime.now().strftime('%Y.%m.%d')                     #Variable for todays date YYYY/MM/DD
+        timeNow = datetime.datetime.now().strftime('%H:%M:%S')                  #Variable for current time HH:MM:SS
+        msg.payload = msg.payload.decode("utf-8")                               #Allows message to contain special characters
 
-        sql = "INSERT INTO test (Date, Time, randNumber) VALUES (%s, %s, %s)"   #Tabel: test; Columns: Date,Time,randNumber
-        val = (date, timeNow, int(msg.payload))       #Date: YYYY/MM/DD
+        sql = "INSERT INTO test (Date, Time, randNumber) VALUES (%s, %s, %s)"   #Tabelname: test; Columns: Date,Time,randNumber
+        val = (date, timeNow, int(msg.payload))                                 #Date: YYYY/MM/DD
         mycursor.execute(sql, val)
-        print("[" + msg.topic + "] " + timeNow +" -  " +  str(msg.payload))
+        print("[" + msg.topic + "] " + timeNow +" -  " +  str(msg.payload))     #Prints Channel, Date+Time, Message into console
 
-        mydb.commit()
+        mydb.commit()                                                           #Sends data to Database
         print("[" + msg.topic + "] " + timeNow +" - " + mycursor.rowcount + "record inserted."  +  str(msg.payload))
 
 client = mqtt.Client()
@@ -41,4 +41,4 @@ client.on_message = on_message
 
 client.connect(MQTT_SERVER, 1883, 60)
 
-client.loop_forever()
+client.loop_forever()                                                           #Program keeps waiting for messages
